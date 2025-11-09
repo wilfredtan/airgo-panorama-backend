4572cd83efc61e73ede9315d6a174395c40626ec
Wilfred Tan <23373389+wilfredtan@users.noreply.github.com>
2025-11-09 22:19:31 +0800

    Use async handler

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,15 @@
 import serverlessExpress from '@vendia/serverless-express';
 import { app } from './app';
 
-// Export the Lambda handler
-export const handler = serverlessExpress({ app });
+// Create the serverless express instance
+const serverlessExpressInstance = serverlessExpress({ app });
+
+// Export the async Lambda handler
+export const handler = async (event: any, context: any) => {
+  return new Promise((resolve, reject) => {
+    serverlessExpressInstance(event, context, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  });
+};
