69f8a1047d914316c70ef0dbcda0ec47f329744f
Wilfred Tan <23373389+wilfredtan@users.noreply.github.com>
2025-11-09 22:11:50 +0800

    Fix undefined lambda handler

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,4 +2,4 @@ import serverlessExpress from '@vendia/serverless-express';
 import { app } from './app';
 
 // Export the Lambda handler
-export const graphqlHandler = serverlessExpress({ app });
+export const handler = serverlessExpress({ app });
